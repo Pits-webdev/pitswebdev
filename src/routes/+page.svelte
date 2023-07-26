90665a5415ee292d55a9d2c8ea8026c4b9286aaf
Pits-webdev <pits.webdev@gmail.com>
2023-07-26 22:52:12 +0200

    add image

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <div class="home container_xl">
 	<h1>Welcome to SvelteKit Github Page</h1>
 	<p>pits-webdev</p>
-	<img src="images/hammer.png" alt="work" />
+	<img src="hammer.png" alt="work" />
 </div>
 
 <style>
