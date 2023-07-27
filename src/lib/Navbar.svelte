b385aca3508b4dcd08c31822acc98b6368974b34
Pits-webdev <pits.webdev@gmail.com>
2023-07-27 18:12:57 +0200

    header top

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -1,11 +1,49 @@
 <script>
+	import logo from '$lib/images/logo.png';
+
 	// /pitswebdev
 </script>
 
-<header>
-	<nav>
-		<p>Home</p>
-		<a href="/pitswebdev">Home</a>
-		<a href="/pitswebdev/about">About</a>
+<header class="header">
+	<nav class="navbar">
+		<div class="logo">
+			<a href="/"><img src={logo} alt="logo ps" /></a>
+		</div>
+
+		<ul class="menu">
+			<li><a href="/pitswebdev">Home</a></li>
+			<li><a href="/pitswebdev/about">About</a></li>
+			<li><a href="/pitswebdev/work">Work</a></li>
+			<li><a class="li_con" href="/pitswebdev/contact">Contact</a></li>
+		</ul>
 	</nav>
 </header>
+
+<style>
+	.header {
+		width: 100%;
+		height: var(--header_height);
+	}
+
+	.navbar {
+		width: 92%;
+		height: 100%;
+		margin: 0 auto;
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+	}
+
+	.menu {
+		display: flex;
+		align-items: center;
+		gap: 6.4rem;
+	}
+
+	.li_con {
+		background-color: var(--yellow);
+		color: var(--dark);
+		padding: 0.4rem 1.6rem;
+		border-radius: 3.2rem;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,24 +1,73 @@
 <script>
 	import work from '../lib/images/hammer.png';
+	import arrow from '$lib/images/arrow.png';
 </script>
 
-<div class="home container_xl">
-	<h1>Welcome to SvelteKit Github Page</h1>
-	<p>pits-webdev</p>
-	<img src={work} alt="work" />
+<div class="head container_xl">
+	<div class="top">
+		<div class="grid">
+			<div class="squer">
+				<span class="icon">ps</span>
+				<span class="icon">
+					<img src={arrow} alt="arrow" />
+				</span>
+			</div>
+			<div class="hl_top">
+				<h1>FUTURE <span>DIGITAL</span></h1>
+			</div>
+
+			<div class="hl_bottom">
+				<h1><span>UNIQUE</span> WEBDESIGN</h1>
+			</div>
+		</div>
+	</div>
 </div>
 
 <style>
-	.home {
+	.head {
 		display: flex;
 		flex-direction: column;
 		align-items: center;
 	}
-	p {
-		margin-top: 3.2rem;
+	.top {
+		width: 100%;
+		height: 100%;
+		display: flex;
+		gap: 3.2rem;
 	}
-	img {
-		width: 200px;
-		margin-top: 20rem;
+
+	.squer {
+		max-width: 22vw;
+		height: 82%;
+		background-color: var(--gray);
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		gap: 3.2rem;
+		border-radius: 6.4rem;
+		padding: 1.6rem 8rem;
+	}
+
+	.icon {
+		display: inline-block;
+	}
+
+	.grid {
+		display: grid;
+		grid-template-columns: repeat(7, minmax(0, 1fr));
+		grid-template-rows: repeat(2, minmax(0, 1fr));
+		align-items: center;
+	}
+
+	.squer {
+		grid-column: 1 / 3;
+	}
+
+	.hl_top {
+		grid-column: 3 / -1;
+	}
+
+	.hl_bottom {
+		grid-column: 1 / -1;
 	}
 </style>
